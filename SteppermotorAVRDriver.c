a3056563fafa343bf81cb084fa27898ecf504be8
Fender <visser.roy@mail.com>
2024-05-28 14:35:53 +0200

    Working commit
    Working standalone commit

---
+++ SteppermotorAVRDriver.c
@@ -0,0 +1,139 @@
+/*
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/delay.h>
+#include "SteppermotorAVRDriver.h"
+
+
+
+//Motor loop's
+#define LoopLength 8
+int LoopForward[LoopLength] = {
+    0b0001, // IN1
+    0b0011, // IN1 + IN2
+    0b0010, // IN2
+    0b0110, // IN2 + IN3
+    0b0100, // IN3
+    0b1100, // IN3 + IN4
+    0b1000, // IN4
+    0b1001  // IN4 + IN1
+};
+int LoopBackwards[LoopLength] = {
+    0b1001, // IN4 + IN1
+    0b1000, // IN4
+    0b1100, // IN3 + IN4
+    0b0100, // IN3
+    0b0110, // IN2 + IN3
+    0b0010, // IN2
+    0b0011, // IN1 + IN2
+    0b0001  // IN1
+};
+int (*leftMotorLoop)[LoopLength] = &LoopForward;
+int (*rightMotorLoop)[LoopLength] = &LoopForward;
+
+
+
+int LeftStepperSpeed = 0;
+int RightStepperSpeed = 0;
+int leftMotorEnabled = 1;
+int rightMotorEnabled = 1;
+
+
+void setBothStepperMode(int mode){
+    setStepperMode(leftMotor, mode);
+    setStepperMode(rightMotor, mode);
+}
+
+void setStepperMode(int motor, int mode){
+
+    int state;
+    if(mode == Off){
+        state = 0;
+    } else {
+        state = 1;
+    }
+
+
+    int** motorLoop;
+    switch(motor){
+            case leftMotor:
+            motorLoop = &leftMotorLoop;
+            leftMotorEnabled = state;
+            break;
+        case rightMotor:
+            motorLoop = &rightMotorLoop;
+            rightMotorEnabled = state;
+            break;
+    }
+
+    switch(mode){
+        default:
+        case ForwardStep:
+            *motorLoop = &LoopForward;
+            break;
+        case BackwardStep:
+            *motorLoop = &LoopBackwards;
+            break;
+    }
+}
+
+//Clockspeed = 16.000.000 / 250 / 64 = 1.000
+#define Clockspeed 1000 //wrong but stfu lmao
+void init_timer(){
+    // Use mode 0, clkdiv = 64
+    TCCR0A = 0;
+    TCCR0B = (0<<CS02) | (1<<CS01) | (1<<CS00);
+
+    // Disable PWM output
+    OCR0A = 0;
+    OCR0B = 0;
+
+    //Overflow interrupt
+    TIMSK0 = (1<<TOIE0);
+    TCNT0 = 6;
+
+    sei();
+}
+
+void initSteppermotorAVRDriver(){
+    initMotorpins();
+    setStepperMode(leftMotor, ForwardStep);
+    init_timer();
+}
+
+
+void initMotorpins(){
+    for(int i = MotorLeftStartpin; i < MotorLeftStartpin+4; i++){
+        MotorLeftDDR |= (1<<i);
+    }
+
+    for(int i = MotorRightStartpin; i < MotorRightStartpin+4; i++){
+        MotorRightDDR |= (1<<i);
+    }
+}
+
+void LeftStepperNextStep(){
+    static int position = 0;
+    MotorLeftPort &= ~(0b1111<< MotorLeftStartpin);
+    MotorLeftPort |= ((*leftMotorLoop)[position] << MotorLeftStartpin);
+    position++;
+    if(position == LoopLength) position = 0;
+}
+
+void RightStepperNextStep(){
+    static int position = 0;
+    MotorRightPort &= ~(0b1111<<MotorRightStartpin);
+    MotorRightPort |= ((*rightMotorLoop)[position] << MotorRightStartpin);
+    position++;
+    if(position == LoopLength) position = 0;
+}
+
+
+ISR(TIMER0_OVF_vect){\
+    if(leftMotorEnabled) LeftStepperNextStep();
+    if(rightMotorEnabled) RightStepperNextStep();
+
+    TCNT0 = 6;
+}
--- main.c
+++ main.c
@@ -6,14 +6,70 @@
  */
 
 #include <avr/io.h>
+#include "SteppermotorAVRDriver.h"
+#include "Ultrasone_sensor.h"
+
+#define distanceToFollow 20
+#define dAccuracy 3
+
+
+#define minDistance (distanceToFollow - dAccuracy)
+#define maxDistance (distanceToFollow + dAccuracy)
 
 int main(void)
 {
 
-    // Insert code
+    DDRB |= (1 << PB7);
+    DDRB |= (1 << PB6);
 
-    while(1)
-    ;
+    // Insert code
+    agv_ultrasoon_init();
+    initSteppermotorAVRDriver();
+
+    int distance;
+    while(1){
+        distance = agv_ultrasoon_boom_links;
+
+        followHand(filterDistance(distance));
+
+        if(distance > distanceToFollow){
+            PORTB |= (1<<PB7);
+        } else {
+            PORTB &= ~(1<<PB7);
+        }
+
+        if(distance < distanceToFollow){
+           PORTB |= (1<<PB6);
+        } else {
+            PORTB &= ~(1<<PB6);
+        }
+    }
 
     return 0;
 }
+
+int filterDistance(int distance){
+    if(distance > 200){
+        distance = 1;
+    }
+    return distance;
+}
+
+void followHand(int distance){
+    if((distance < maxDistance) && (distance > minDistance)){
+            setBothStepperMode(Off);
+            return;
+    }
+
+    if(distance < minDistance){
+        setBothStepperMode(BackwardStep);
+        return;
+    }
+
+    if(distance > maxDistance){
+        setBothStepperMode(ForwardStep);
+    }
+
+}
+
+
