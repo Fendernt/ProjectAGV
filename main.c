d4f83faa383910ff756a7777efe1dd5a34d46226
Fender <visser.roy@mail.com>
2024-05-28 13:51:46 +0200

    Sonic sensor test
    Working standalone version of the sonic sensor

--- main.c
+++ main.c
@@ -6,14 +6,48 @@
  */
 
 #include <avr/io.h>
+#include "Ultrasone_sensor.h"
+
+
+
+#define distanceToFollow 20
+#define dAccuracy 3
+
+
+#define minDistance (distanceToFollow - dAccuracy)
+#define maxDistance (distanceToFollow + dAccuracy)
+
 
 int main(void)
 {
 
     // Insert code
+    agv_ultrasoon_init();
 
-    while(1)
-    ;
+
+    DDRB |= (1 << PB7);
+    DDRB |= (1 << PB6);
+
+    int distance;
+
+    while(1) {
+
+        distance = agv_ultrasoon_boom_links;
+
+        //followHand(distance);
+
+        if(distance > distanceToFollow){
+            PORTB |= (1<<PB7);
+        } else {
+            PORTB &= ~(1<<PB7);
+        }
+
+        if(distance < distanceToFollow){
+           PORTB |= (1<<PB6);
+        } else {
+            PORTB &= ~(1<<PB6);
+        }
+    }
 
     return 0;
 }
