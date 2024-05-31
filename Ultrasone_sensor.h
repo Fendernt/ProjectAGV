26a315143e29ae2c06451791cffd21f38ea9efdd
Fender <visser.roy@mail.com>
2024-05-31 15:52:55 +0200

    added some comments for pins

--- Ultrasone_sensor.h
+++ Ultrasone_sensor.h
@@ -1,11 +1,10 @@
 #ifndef AGV_ULTRASOON_H_INCLUDED
 #define AGV_ULTRASOON_H_INCLUDED
 
-extern volatile uint16_t agv_ultrasoon_boom_links;
-extern volatile uint16_t agv_ultrasoon_boom_rechts;
-extern volatile uint16_t agv_ultrasoon_voor_links;
-extern volatile uint16_t agv_ultrasoon_voor_midden;
-extern volatile uint16_t agv_ultrasoon_voor_rechts;
+//Trigger pin allemaal op pin 5 via de connector
+extern volatile uint16_t agv_ultrasoon_boom_links; //ECHO op Pin A10
+extern volatile uint16_t agv_ultrasoon_boom_rechts; //ECHO op Pin A9
+extern volatile uint16_t agv_ultrasoon_voor_midden; //ECHO op Pin A12
 void agv_ultrasoon_init();
 
 #endif // AGV_ULTRASOON_H_INCLUDED
