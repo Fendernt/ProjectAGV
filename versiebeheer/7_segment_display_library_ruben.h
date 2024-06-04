c187559cdc3c6b363e64928cbdf6a4624464627d
Edery10 <67152221+Edery10@users.noreply.github.com>
2024-06-04 13:09:35 +0200

    versiebeheer_Ruben

---
+++ versiebeheer/7_segment_display_library_ruben.h
@@ -0,0 +1,63 @@
+//Hoofdletters
+int disp_A = 0x88;
+int disp_B = 0x80;
+int disp_C = 0xC6;
+int disp_D = 0xC0;
+int disp_E = 0x86;
+int disp_F = 0x8E;
+int disp_H = 0x89;
+int disp_I = 0xF9;
+int disp_J = 0xE0;
+int disp_L = 0xC7;
+int disp_O = 0xC0;
+int disp_P = 0x8C;
+int disp_S = 0x92;
+int disp_U = 0xC1;
+int disp_Y = 0x91;
+int disp_Z = 0xA4;
+
+//Kleine letters
+int disp_b = 0x83;
+int disp_c = 0xA7;
+int disp_d = 0xA1;
+int disp_f = 0x8E;
+int disp_g = 0x90;
+int disp_h = 0x8B;
+int disp_i = 0xF9;
+int disp_j = 0xF1;
+int disp_l = 0xC7;
+int disp_n = 0xAB;
+int disp_o = 0xA3;
+int disp_p = 0x8C;
+int disp_q = 0x98;
+int disp_r = 0xCE;
+int disp_s = 0x92;
+int disp_t = 0x87;
+int disp_u = 0xE3;
+int disp_y = 0x91;
+int disp_z = 0xA4;
+
+//Cijfers
+int disp_0 = 0xC0;
+int disp_1 = 0xF9;
+int disp_2 = 0xA4;
+int disp_3 = 0xB0;
+int disp_4 = 0x99;
+int disp_5 = 0x92;
+int disp_6 = 0x82;
+int disp_7 = 0xF8;
+int disp_8 = 0x80;
+int disp_9 = 0x90;
+
+//Andere karakters
+int disp_clear = 0xFF;
+int disp_min = 0xBF;
+int disp_is = 0xB7;
+int disp_line_A = 0xFE;
+int disp_line_B = 0xFD;
+int disp_line_C = 0xFB;
+int disp_line_D = 0xF7;
+int disp_line_E = 0xEF;
+int disp_line_F = 0xDF;
+int disp_line_G = 0xBF;
+int disp_line_H = 0x7F;
---
+++ versiebeheer/ruben.c
@@ -0,0 +1,76 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include "7_segment_display_library_ruben.h"
+
+#define Shiftclk PH4
+#define Latchclk PG5
+#define SDI PH5
+
+void init(void) {
+    // Initialiseer de pinnen voor datain, shiftclk en latchclk als output
+    DDRG |= (1 << Latchclk);
+    DDRH |= (1 << Shiftclk);
+    DDRH |= (1 << SDI);
+
+    // Maak shiftclk en latchclk laag
+    PORTH &= ~(1 << Shiftclk);
+    PORTG &= ~(1 << Latchclk);
+}
+
+void send_data(int data) {
+    // data van karakter
+    for (int i = 7; i >= 0; i--) {
+        if ((data & (1 << i)) == 0) {
+            PORTH &= ~(1 << SDI);
+        } else {
+            PORTH |= (1 << SDI);
+        }
+
+        PORTH ^= (1 << Shiftclk);
+        PORTH ^= (1 << Shiftclk);
+    }
+}
+
+void send_enable(int data) {
+    send_data(data);
+}
+
+void display(int data, int disp) {
+    send_data(data);
+    send_enable(disp);
+
+    PORTG ^= (1 << Latchclk);
+    PORTG ^= (1 << Latchclk);
+}
+
+int main(void) {
+    init();
+    while (1) {
+        for (int digit = 0; digit <= 3; digit++) {
+            int pattern;
+            int place;
+            switch (digit) {
+            case 0:
+                pattern = disp_1;
+                place = 1; //Links
+                break;
+            case 1:
+                pattern = disp_2;
+                place = 2; //Midden-links
+                break;
+            case 2:
+                pattern = disp_3;
+                place = 4; //Midden-rechts
+                break;
+            case 3:
+                pattern = disp_4;
+                place = 8; //Rechts
+                break;
+            default:
+                pattern = disp_clear;
+            }
+            display(pattern, place);
+            //_delay_ms(100);
+        }
+    }
+}
