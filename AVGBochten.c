e2a18dee90d752e828597e2202a03c13326a7563
Fender <visser.roy@mail.com>
2024-06-04 10:09:38 +0200

    comments

--- AVGBochten.c
+++ AVGBochten.c
@@ -6,6 +6,22 @@
 #include "AGVBochten.h"
 #include "SteppermotorAVRDriver.h"
 
+/*
+    Werking:
+
+    Zet timer aan en rij vooruit
+    compA om de bocht te beginnen
+    CompB om de bocht te eindigen
+
+    reset timer en rij vooruit
+    CompA voor begin bohct
+    CompB voor eind bocht
+
+    rij vooruit
+
+    halve draai gemaakt
+*/
+
 //1 sec = 30
 #define second 30
 #define Comp_StartTurn (second*3)
@@ -15,43 +31,48 @@
 #define Left 0
 #define Right 1
 
-
+//Counter
 volatile int counter = 0;
 
 
-int turnStarted = 0;
-int turnsTaken = 0;
+int turnStarted = 0; //Check voor als we in een bocht zitten
+int turnsTaken = 0; //Is er om volgorde bij te houden.
 
+//Direction om bocht te maken
 int turnDirection = 0;
 
-
+//Init
 void initAGVBochten(){
     init_AGVBochten_timer();
 }
 
 
+//Start turn
 int startTurn(int direction){
-    if(turnStarted == 1) {
+    if(turnStarted == 1) { //Check of we niet al een bocht aan het maken zijn
         return checkIfFinished();
     }
 
 
     turnStarted = 1;
     turnDirection = direction;
-    startTimer();
+    startTimer(); //Start de bocht
 }
 
+//Set timer aan
 void startTimer(){
     counter = 0;
     TCNT4 = 0;
     TCCR4B = (0<<CS42) | (1<<CS41) | (0<<CS40);
 }
 
+//Stop timer
 void stopTimer(){
      TCCR4B = (0<<CS42) | (0<<CS41) | (0<<CS40);
      turnsTaken++;
 }
 
+//Check of de bocht af is
 void checkIfFinished(){
     if(turnsTaken >= 3){
         turnsTaken = 0;
@@ -79,19 +100,22 @@ void init_AGVBochten_timer(){
 ISR(TIMER4_OVF_vect){
     counter++;
 
+    //Begin bocht
     if(counter == Comp_StartTurn){
-        if(turnsTaken == 2) {
+        if(turnsTaken == 2) { //Als we al 2 bochten hebben gemaakt stop de timer
             stopTimer();
-        } else turn(turnDirection);
+        } else turn(turnDirection); //Zoniet, maak de bocht
     }
 
+    //Stop de bocht
     if(counter == Comp_EndTurn){
         setBothStepperMode(ForwardStep);
-        if(turnsTaken == 0) counter = DrivingHeadstart;
-        turnsTaken++;
+        if(turnsTaken == 0) counter = DrivingHeadstart; //Als dit het einde is van de eerste bocht, reset de timer om het te herhalen
+        turnsTaken++; //increase aantal bochten
     }
 }
 
+//Zet de wielen goed voor de bocht
 void turn(int direction){
     switch(direction) {
     case Left:
