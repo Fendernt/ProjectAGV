ddaa0d0aa623ac04d588146aa5ba5635596a967b
Japsinator <23049642@student.hhs.nl>
2024-06-05 13:50:03 +0200

    Dummy project

---
+++ versiebeheer/jasper.c
@@ -0,0 +1,32 @@
+/*
+ */
+
+#include <avr/io.h>
+
+#define MAGNET PB7
+#define Z_AS_BOTTOM PF1
+
+int MAGNET_PRESSED = 0;
+
+int main(void)
+{
+     DDRB |= (1 << MAGNET);
+     DDRF |= (1 << Z_AS_BOTTOM);
+     PORTF |= (1 << Z_AS_BOTTOM);
+     PORTB |= (1 << MAGNET);
+
+   while(1){
+        if(!(PINF & (1 << Z_AS_BOTTOM)))
+        {
+            if(MAGNET_PRESSED == 0)
+            {
+                MAGNET_PRESSED = 1;
+                PORTB &= ~(1 << MAGNET);
+            }
+        } else {
+            MAGNET_PRESSED = 0;
+        }
+   }
+
+    return 0;
+}
