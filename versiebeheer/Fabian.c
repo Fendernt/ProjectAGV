48d675bb07e6f135b59b46ace226dbcb9ca6384e
TaiLaLa <f.molenaar14@gmail.com>
2024-06-06 09:55:27 +0200

    Add files via upload

---
+++ versiebeheer/Fabian.c
@@ -0,0 +1,66 @@
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+ISR(TIMER0_OVF_vect)
+{
+    PORTB &= ~(1<<PB7);
+    int x = 1;
+
+    if(x == 1)
+    {
+        OCR0A++;
+        if(OCR0A > 255)
+        {
+            x = 0;
+        }
+    }
+    else if(x == 0)
+    {
+        OCR0A--;
+        if(OCR0A < 1)
+        {
+            x = 1;
+        }
+    }
+
+}
+
+ISR(TIMER0_COMPA_vect)
+{
+    PORTB |= (1<<PB7);
+}
+
+
+void init_led(void)
+{
+    DDRB |= (1<<PB7);
+    PORTB &= ~(1<<PB7);
+}
+
+void init_timer(void)
+{
+    TCCR0A = 0;
+    TCCR0B |= (1<<CS02) | (0<<CS01) | (1<<CS00);
+
+}
+
+void init_interrupt(void)
+{
+    TIMSK0 |= (1<<TOIE0) | (1<<OCIE0A);
+    OCR0A = 1;
+}
+
+int main(void)
+{
+
+    init_led();
+    init_timer();
+    init_interrupt();
+    sei();
+
+    while(1)
+    ;
+
+    return 0;
+}
